ab5d77efddf0ecd5d7e67ae76c613aafce35717a
daeunii94 <39181911+daeunii94@users.noreply.github.com>
2018-07-21 15:01:18 +0900

    FormView2 modified
    1. add FormView
    2. MainController.js - default add onSubmit()

--- 1-vanilla/js/controllers/MainController.js
+++ 1-vanilla/js/controllers/MainController.js
@@ -1,10 +1,14 @@
-import FormView from '../views.FormView.js'
+import FormView from '../views/FormView.js'
 
 const tag = '[MainController]'
 
-export default { 
-    init() {
-        console.log(tag, 'init()')
-        FormView.setup(document.querySelector('form'))
-    }
+export default {
+  init() {
+    FormView.setup(document.querySelector('form'))
+      .on('@submit', e => this.onSubmit(e.detail.input))
+  },
+  
+  onSubmit(input) {
+    console.log(tag, 'onSubmit()', input)
+  },
 }
