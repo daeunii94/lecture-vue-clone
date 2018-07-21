68a547cbdf944147d143229198f88776f6b12a96
daeunii94 <39181911+daeunii94@users.noreply.github.com>
2018-07-21 15:37:34 +0900

    FormView3 add reset
    1. reset에 addEventListener 등록
    
    2. onClickReset() 구현
    
    3. inputEl.value > 0 => emit('@reset')
    
    4. Controller onResetForm() => print log

--- 1-vanilla/js/views/FormView.js
+++ 1-vanilla/js/views/FormView.js
@@ -20,13 +20,21 @@ FormView.showResetBtn = function (show = true) {
 FormView.bindEvents = function () {
   this.on('submit', e => e.preventDefault())
   this.inputEl.addEventListener('keyup', e => this.onKeyup(e))
+  this.resetEl.addEventListener('click', e => this.onClickReset())
+
 }
 
 FormView.onKeyup = function (e) {
   const enter = 13
   this.showResetBtn(this.inputEl.value.length)
+  if(this.inputEl.value.length) this.emit('@reset')
   if (e.keyCode !== enter) return
-  this.emit('@submit', { input: this.inputEl.value })
+  this.emit('@submit', { input: this.inputEl.value }) //엔터 입력하면 검색결과 보이게 컨트롤러에게 위임만 함
+}
+
+FormView.onClickReset = function() {
+  this.emit('@reset')
+  this.showResetBtn(false)
 }
 
 export default FormView
