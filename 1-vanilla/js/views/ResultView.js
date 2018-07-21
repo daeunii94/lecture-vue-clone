2535651bef323864ebd6c276a945a9f7c0e0b9d2
daeunii94 <39181911+daeunii94@users.noreply.github.com>
2018-07-21 17:29:39 +0900

    ResultView1 add ResultView
    1. index.html
    add search-result
    
    2. add ResultView
    - setup() ->init()
    - render() ->
    has data->getSearchResultHtml(data)
    - getSearchResultHtml()->debug
    
    2.Controller
    FormView->ResultView

---
+++ 1-vanilla/js/views/ResultView.js
@@ -0,0 +1,24 @@
+import View from './View.js'
+
+const tag = '[ResultView]'
+
+const ResultView = Object.create(View)
+
+ResultView.messages = {
+    NO_RESULT: '검색 결과가 없습니다'
+}
+ResultView.setup = function(el) {
+    this.init(el)
+}
+
+ResultView.render = function(data = []) {
+    console.log(tag, 'render()', data)
+    this.el.innerHTML = data.length ? this.getSearchResultHtml(data) : this.messages.NO_RESULT
+    this.show()
+}
+
+ResultView.getSearchResultHtml = function (data) {
+    debugger
+}
+
+export default ResultView
