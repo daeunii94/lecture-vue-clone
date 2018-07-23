44e5ea28e50e404dda06f9b60849c16f1c64cc55
daeunii94 <39181911+daeunii94@users.noreply.github.com>
2018-07-23 21:04:26 +0900

    KeywordView1 add KeywordView
    1. index.html
    -add search-keyword element
    
    2. add KeywordView
    -setup() -> get element
    -render() -> show()
    -getKeywordHtml -> if data, return html
    
    3.Controller
    - setup() -> get seach-keyword
    -fetchSearchKeyword() ->
                                model->data

--- 1-vanilla/js/controllers/MainController.js
+++ 1-vanilla/js/controllers/MainController.js
@@ -2,8 +2,10 @@ import FormView from '../views/FormView.js'
 import ResultView from '../views/ResultView.js'
 import TabView from '../views/TabView.js'
 
+import KeywordView from '../views/KeywordView.js'
 
 import SearchModel from '../models/SearchModel.js'
+import KeywordModel from '../models/KeywordModel.js'
 
 const tag = '[MainController]'
 
@@ -16,8 +18,10 @@ export default {
     TabView.setup(document.querySelector('#tabs'))
       .on('@change', e => this.onChangeTab(e.detail.tabName))
     
-      ResultView.setup(document.querySelector('#search-result'))
+    ResultView.setup(document.querySelector('#search-result'))
+    KeywordView.setup(document.querySelector('#search-keyword'))
 
+    
     this.selectedTab = '추천 검색어'
     this.renderView()
     
@@ -26,7 +30,24 @@ export default {
   renderView() {
     console.log(tag, 'renderView()')
     TabView.setActiveTab(this.selectedTab)
+
+    if (this.selectedTab === '추천 검색어') {
+      this.fetchSearchKeyword()
+     
+    } else {
+      
+
+    }
+
+
     ResultView.hide()
+
+  },
+
+  fetchSearchKeyword() {
+    KeywordModel.list().then(data => {
+      KeywordView.render(data)
+    })
 
   },
   
