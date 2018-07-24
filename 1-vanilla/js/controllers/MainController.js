6321d3e42e00127feab9b49648ab362bd6667e8d
daeunii94 <39181911+daeunii94@users.noreply.github.com>
2018-07-24 13:43:44 +0900

    HistoryView1 add HistoryView
    1.index.html
    -add search-history element
    
    2. add HistoryView
    -copy KeywordView
    
    3. Controller
    -init() -> setup() -> register click event
    -onClickHistory() -> search()
    -renderView() -> fetchSearchHistory() ->
    render HistoryView

--- 1-vanilla/js/controllers/MainController.js
+++ 1-vanilla/js/controllers/MainController.js
@@ -2,9 +2,11 @@ import FormView from '../views/FormView.js'
 import ResultView from '../views/ResultView.js'
 import TabView from '../views/TabView.js'
 import KeywordView from '../views/KeywordView.js'
+import HistoryView from '../views/HistoryView.js'
 
 import SearchModel from '../models/SearchModel.js'
 import KeywordModel from '../models/KeywordModel.js'
+import HistoryModel from '../models/HistoryModel.js'
 
 const tag = '[MainController]'
 
@@ -18,10 +20,14 @@ export default {
       .on('@change', e => this.onChangeTab(e.detail.tabName))
     
     ResultView.setup(document.querySelector('#search-result'))
+
     KeywordView.setup(document.querySelector('#search-keyword'))
       .on('@click', e => this.onClickKeyword(e.detail.keyword))
+
+    HistoryView.setup(document.querySelector('#search-history'))
+      .on('@click', e => this.onClickHistory(e.detail.history))
     
-    this.selectedTab = '추천 검색어'
+    this.selectedTab = '최근 검색어'
     this.renderView()
     
   },
@@ -33,7 +39,7 @@ export default {
     if (this.selectedTab === '추천 검색어') {
       this.fetchSearchKeyword()
     } else {
-      debugger
+      this.fetchSearchHistory()
     }
     ResultView.hide()
 
@@ -42,6 +48,12 @@ export default {
   fetchSearchKeyword() {
     KeywordModel.list().then(data => {
       KeywordView.render(data)
+    })
+  },
+
+  fetchSearchHistory() {
+    HistoryModel.list().then(data => {//HistoryModel에서 data 가져옴
+      HistoryView.render(data)//KeywordView의 render호출
     })
   },
   
@@ -77,5 +89,10 @@ export default {
 
   onClickKeyword(keyword) {
     this.search(keyword)
+  },
+
+  onClickHistory(keyword) {
+    this.search(keyword)
   }
+
 }
