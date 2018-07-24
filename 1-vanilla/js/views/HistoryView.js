3d16e54f285ddae9efd39435a3d09cf0b6a2e3b0
daeunii94 <39181911+daeunii94@users.noreply.github.com>
2018-07-24 14:26:14 +0900

    HistoryView2 print date, x button
    1. HistoryView
    -getKeywordHtml() override
            -> add date, btn in html

--- 1-vanilla/js/views/HistoryView.js
+++ 1-vanilla/js/views/HistoryView.js
@@ -4,4 +4,16 @@ const tag = '[HistoryView]'
 
 const HistoryView = Object.create(KeywordView)
 
+HistoryView.messages.NO_KEYWORDS = '검색 이력이 없습니다'
+
+HistoryView.getKeywordHtml = function (data) {
+    return data.reduce((html, item) => {
+      html += `<li data-keyword="${item.keyword}">
+        ${item.keyword} 
+        <span class="date">${item.date}</span>
+        <button class="btn-remove"></button>
+      return html
+    }, '<ul class="list">') + "</ul>"
+}
+
 export default HistoryView
