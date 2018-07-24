f377e6d806ad6831a6f26e1f3b6c1c17a8c5b9f3
daeunii94 <39181911+daeunii94@users.noreply.github.com>
2018-07-25 01:55:46 +0900

    scafolding base methods
    1. app.js
    -add  base methods

--- 2-vue/js/app.js
+++ 2-vue/js/app.js
@@ -1,10 +1,31 @@
+import SearchModel from './models/SearchModel.js'
+
 new Vue({
     el: '#app',
     data: {
-        query : '' //입력 데이터 받아서 저장
+        query : '', //입력 데이터 받아서 저장
+        searchResult: []
+    },
     methods: {
         onSubmit(e) {
-            
+            this.search()
+        },
+        onKeyup() { 
+            if (!this.query.length) this.onReset() //입력값이 없을경우,빌 경우, onReset
+        },
+        onReset() {
+            this.resetForm()
+  
+        },
+        search() {
+            SearchModel.list().then(data => {
+                this.searchResult = data
+            })
+        },
+        resetForm() {
+            this.query = '' 
+            //todo remove results ..
+            debugger
         }
     }
 })
